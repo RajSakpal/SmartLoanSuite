4ed42c9b7db383a368be5274043c556a0941241c
Raj Sakpal <rajsakpal2000@gmail.com>
2025-07-05 21:49:09 +0530

    Fixed mail config, email now sent successfully.

---
+++ user-service/user-service/src/main/java/com/smartloansuite/user_service/config/MailConfig.java
@@ -0,0 +1,39 @@
+package com.smartloansuite.user_service.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Value("${mail.username}") // Get from application.yml
+    private String username;
+
+    @Value("${mail.password}") // Get from application.yml
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("sandbox.smtp.mailtrap.io");
+        mailSender.setPort(2525);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true"); // Important addition
+        props.put("mail.smtp.connectiontimeout", 5000);
+        props.put("mail.smtp.timeout", 5000);
+        props.put("mail.smtp.writetimeout", 5000);
+
+        return mailSender;
+    }
+}
+
